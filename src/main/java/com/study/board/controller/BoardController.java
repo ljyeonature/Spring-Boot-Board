428a10ab7cfd9f18dfcefa749fac21def20394ac
ljyeonature <ljyeon131@gmail.com>
2023-03-12 10:48:15 +0900

    Write board form

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,16 +1,15 @@
 package com.study.board.controller;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardWriteForm() {
 
-        return "Hello World";
+        return "boardwrite";
     }
 }
